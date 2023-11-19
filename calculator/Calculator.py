32f1c1ac4bb4bdb114e9eb8ae5f85e9127fc4316
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2023-11-20 00:20:11 +0200

    Add Calculator Class Methods

--- calculator/Calculator.py
+++ calculator/Calculator.py
@@ -1,12 +1,18 @@
 #!bin/python3
 
 class Calculator:
-    def __init__(self, x: int, y: int) -> None:
+    def __init__(self, x: float | int, y: float | int) -> None:
         self.x = x
         self.y = y
     
-    def add(self, x: float, y: float) -> float:
+    def add(self, x: float | int, y: float | int) -> float | int:
         return self.x + self.y
+
+    def minus(self, x: float | int, y: float | int) -> float | int:
+        return self.x - self.y
     
-    def add(self, x: int, y: int) -> int:
-        return self.x + self.y
+    def div(self, x: float | int, y: float | int) -> float | int:
+        if y != 0:
+            return self.x - self.y
+        else:
+            print("Error")
