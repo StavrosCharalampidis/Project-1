1a9623a5027595e15f193d038190dc38acc06642
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2023-11-20 02:20:12 +0200

    Main App

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 #!bin/python3
 
+from menu.Menu import Menu
 from calculator import Calculator
-
 x: int = 0
 y: int = 1
 
 calculator = Calculator(x, y)
-print(calculator.div(x, y))
+option = "+"
+M = Menu(option, x, y)
+
+M.menu(option, x, y)
+
+#print(Calculator("+", x, y))
+#print(calculator.add(x, y))
